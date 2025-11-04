e3a2d79a89dc02a8f1da47706b57f9c2cdb6a0b7
mazharinandrei <79073599+mazharinandrei@users.noreply.github.com>
2025-11-04 21:04:29 +0700

    chore: clean-up code
    - delete unused imports
    - delete the unused render_position function
    - delete a method that does nothing

--- project/staff/views.py
+++ project/staff/views.py
@@ -1,7 +1,5 @@
-from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.forms import AuthenticationForm
 
-from django.shortcuts import render
 from django.urls import reverse_lazy
 
 from project.views import (
@@ -14,12 +12,6 @@ from .models import Staff, Positions
 from contracts.models import Contract
 
 from django.contrib.auth.views import LoginView
-
-# Create your views here.
-
-"""
-LIST VIEWS 
-"""
 
 
 class StaffListView(ProjectBaseListView):
@@ -39,8 +31,6 @@ class StaffCreateView(
 
 
 class PositionCreateView(ProjectBaseCreateView):
-    "staff.add_position"
-
     model = Positions
 
 
@@ -57,25 +47,8 @@ class StaffDetailView(ProjectBaseDetailView):
         return context
 
 
-@permission_required("staff.view_position")
-def render_position(request, position_id):
-    position = Positions.objects.get(id=position_id)
-    staffs = Staff.objects.filter(position=position)
-    context = {
-        "title": f"Информация о должности {position.name}",
-        "position": position,
-        "staffs": staffs,
-    }
-    return render(request, "position.html", context)
-
-
 class PositionDetailView(ProjectBaseDetailView):
     model = Positions
-
-    def get_context_data(self, **kwargs):
-        con = super().get_context_data(**kwargs)
-        print(self.__dict__)
-        return con
 
 
 class LoginUser(LoginView):
